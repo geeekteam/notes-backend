de2ba5b3048c08b5389e15017d7eeca6d4efb596
Stas <stas@example.com>
2025-05-09 15:47:10 +0500

    Добавлена регистронезависимость

--- src/Thermometer.java
+++ src/Thermometer.java
@@ -11,16 +11,16 @@ public class Thermometer {
         scanner.nextLine();
         String measure = scanner.nextLine();
         System.out.println(measure);
-        if (measure.equals("C")) {
+        if (measure.equalsIgnoreCase("C")) {
             double result = (temperature*9/5)+32;
             System.out.println("Вы ввели градусы по Цельсию, будет осуществлена конвертация в Фаренгейты");
             System.out.println(temperature + " градусов по Цельсию, составляет " + result + " градусов по Фаренгейту");
-        } else if (measure.equals("F")) {
+        } else if (measure.equalsIgnoreCase("F")) {
             double result = (temperature-32)*5/9;
             System.out.println("Вы ввели градусы по Фаренгейту, будет осуществлена конвертация в Цельсия");
             System.out.println(temperature + " градусов по Фаренгейту, составляет " + result + " градусов по Цельсию");
         } else {
-            System.out.println("Неизвестная единица измерения");
+            System.out.println("Ошибка: допустимы только C или F.");
         }
     }
 }
